ce2c50fc61d1fdaa9bc3f25213e40454ddb1fdf8
Victor Marquez <victmo@gmail.com>
2017-03-23 15:44:20 -0400

    Manually adding cmd+b = bold

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,49 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/* eslint-disable comma-dangle */
+
 import React, {Component} from 'react';
-import {Editor, EditorState} from 'draft-js';
+import {
+  Editor, // The actual editor
+  EditorState, // Editor initial state
+  RichUtils, // So we can bind commands like Cmd+B for bold text
+} from 'draft-js';
+
 
 export default class App extends Component {
 
   constructor( props ) {
     super( props );
 
+
+    // Default state
     this.state = {
       editorState: EditorState.createEmpty()
     };
 
+
+    // 
     this.onChange = ( editorState ) => {
       this.setState( { editorState } );
     }
 
-    this.onSetCustomState = ( event ) => {
-      const myState = { hi: 'Vic' };
-      console.info( 'Custom state!', myState );
+
+
+    this.handleKeyCommand = ( command ) => {
+      console.info( command );
+
+      // Manually bolding
+      if( command === 'bold' ){
+        const newState = RichUtils.toggleInlineStyle( this.state.editorState, 'BOLD' );
+        this.onChange( newState );
+        //this.setState( { editorState: newState } ); // or set it directly
+      }
     }
+
+    // this.onSetCustomState = ( event ) => {
+    //   const myState = { hi: 'Vic' };
+    //   console.info( 'Custom state!', myState );
+    // }
   }
 
 
@@ -30,6 +56,7 @@ export default class App extends Component {
           <Editor
             editorState={ this.state.editorState }
             onChange={ this.onChange }
+            handleKeyCommand={ this.handleKeyCommand }
           />
         </div>
 
