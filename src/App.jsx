b60183fe57f021517c23458e6abdabfed5f383da
Kamari Aykes <kamari.aykes@timeinc.com>
2017-03-23 14:22:37 -0400

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Editor, EditorState } from 'draft-js';
 
 class App extends Component {
+
+constructor( props ) {
+  super( props );
+  this.state = {
+    editorState: EditorState.createEmpty()
+  };
+  
+  this.onChange = ( editorState ) => {
+    this.setState( { editorState } );
+  }
+}
+
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <h1>TfK Editor</h1>
+        <div style={ { border: '1px solid #ddd' } }>
+          <Editor 
+            editorState={ this.state.editorState } 
+            onChange={ this.onChange } 
+          />
+        </div>
+      </div>
     );
   }
 }
